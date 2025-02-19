f3476c60b64df3489caa6c17824640c408363314
AlejandroML <72447092+ALM-Bloom@users.noreply.github.com>
2025-02-19 22:32:38 +0000

    Done another exercise for variables practice.

---
+++ StillUsingVariables/src/Main.java
@@ -0,0 +1,18 @@
+// Write a program that stores your name and year of graduation into variables, 
+// and displays their values on the screen.
+// Make sure that you use two variables, and that the variable that holds your name
+// is the best type for such a variable, and that the variable that holds the year is 
+// the best type for that variable.
+// Also make sure that your variable names are good: the name of a variable should
+// always relate to its contents.
+
+public class Main {
+
+	public static void main(String[] args) {
+      int graduation = 2025;
+      String name = "Alex";
+      System.out.println(name + " will graduate in " + graduation);
+
+	}
+
+}
